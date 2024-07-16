5585f21205ba67b516a7dedaee65e281c3d97f59
alexandergv <69660314+alexandergv@users.noreply.github.com>
2024-07-15 23:16:11 -0400

    config file

---
+++ src/config.js
@@ -0,0 +1,9 @@
+
+const config = {
+    apiUrl: 'http://localhost:3000', // URL base de tu API
+    googleMapsApiKey: 'tu_api_key_de_google_maps',
+    // Otras configuraciones
+  };
+  
+  export default config;
+  
