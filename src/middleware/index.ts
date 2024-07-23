099647b3d5047e367f93268dc776a5ed8d797c91
alexandergv <69660314+alexandergv@users.noreply.github.com>
2024-07-23 05:09:02 -0400

    redirections

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,44 +1,67 @@
-import jwt from 'jsonwebtoken';
-const JWT_SECRET = 'hossio'; 
+// import jwt from 'jsonwebtoken';
+// const JWT_SECRET = 'hossio'; 
 
-function getSession(token: any) {
-    if (token) {
-      try {
-        // Decodifica el JWT y verifica su validez
-        const decoded = jwt.verify(token, JWT_SECRET);
-        return { authenticated: true, user: decoded };
-      } catch (err) {
-        // Token inválido o expirado
-        return { authenticated: false, user: null };
-      }
-    }
+// function getSession(token: any) {
+//     if (token) {
+//       try {
+//         // Decodifica el JWT y verifica su validez
+//         const decoded = jwt.verify(token, JWT_SECRET);
+//         return { authenticated: true, user: decoded };
+//       } catch (err) {
+//         // Token inválido o expirado
+//         return { authenticated: false, user: null };
+//       }
+//     }
     
-    return { authenticated: false, user: null };
-  }
+//     return { authenticated: false, user: null };
+//   }
   
 
-export function onRequest ({ locals, request } : any, next: any) {
-    // interceptar los datos de una solicitud.
-    // opcionalmente, modifica las propiedades en `locals`.
-    let token = request.headers.get('Cookie')?.split(';').find((c :string) => c.trim().startsWith('auth_token='));
-    token = token ? token.split('=')[1] : null
-    const url = new URL(request.url);
-    const pathname = url.pathname;
+// export function onRequest ({ locals, request } : any, next: any) {
+//     // interceptar los datos de una solicitud.
+//     // opcionalmente, modifica las propiedades en `locals`.
+//     let token = request.headers.get('Cookie')?.split(';').find((c :string) => c.trim().startsWith('auth_token='));
+//     token = token ? token.split('=')[1] : null
+//     const url = new URL(request.url);
+//     const pathname = url.pathname;
     
-    const session = getSession(token);
-    locals.isAuthenticated = session.authenticated;
-    locals.user = session.user;
-    console.log(session);
+//     const session = getSession(token);
+//     locals.isAuthenticated = session.authenticated;
+//     locals.user = session.user;
+//     console.log(session);
 
-    if (token && pathname == '/login') {
-        return new Response(null, {
-            status: 302,
-            headers: {
-                'Location': '/'
-            }
-        });
-    }
+//     if (token) {
+//         let Location = '';
+//         if(locals.user?.role == 'owner' && pathname != '/owners') {
+//             Location = '/owners';
+//         } else if (pathname == '/login' && locals.user?.role == 'user') {
+//             Location = '/'
+//         }
 
-    // devuelve una respuesta o el resultado de llamar a `next()`.
-    return next();
-};
+//         if(Location != '') {
+//             return new Response(null, {
+//                 status: 302,
+//                 headers: {
+//                     Location
+//                 }
+//             });
+//         }
+//     } else {
+//         let Location = '';
+//         if(pathname == '/owners') {
+//             Location = '/';
+//         }
+        
+//         if(Location != '') {
+//             return new Response(null, {
+//                 status: 302,
+//                 headers: {
+//                     Location
+//                 }
+//             });
+//         }
+//     }
+
+//     // devuelve una respuesta o el resultado de llamar a `next()`.
+//     return next();
+// };
