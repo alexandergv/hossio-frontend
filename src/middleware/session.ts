b0adf00b780d762046231d7c49966a40aea89862
alexandergv <69660314+alexandergv@users.noreply.github.com>
2024-07-23 11:23:40 -0400

    modificacion de sesion.js a sesion.ts

--- src/middleware/session.ts
+++ src/middleware/session.ts
@@ -5,15 +5,17 @@ import cookie from 'cookie';
 const JWT_SECRET = 'hossio'; // Asegúrate de usar una clave secreta segura y almacenarla de manera segura
 
 // Función para decodificar y verificar el JWT
-export async function getSession(token) {
+export function getSession(token: string | undefined)
+: {authenticated : boolean, user: any} 
+{
   if (token) {
     try {
       // Decodifica el JWT y verifica su validez
       const decoded = jwt.verify(token, JWT_SECRET);
-      console.log(decoded)
       return { authenticated: true, user: decoded };
     } catch (err) {
       // Token inválido o expirado
+      console.error(err);
       return { authenticated: false, user: null };
     }
   }
