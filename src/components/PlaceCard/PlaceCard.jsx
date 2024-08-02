536bfe3ca5d7385012f6b23debe04f37b1460f6c
alexandergv <69660314+alexandergv@users.noreply.github.com>
2024-08-02 19:37:23 -0400

    heart button

--- src/components/PlaceCard/PlaceCard.jsx
+++ src/components/PlaceCard/PlaceCard.jsx
@@ -1,13 +1,25 @@
 import React, { useState } from 'react';
 import './PlaceCard.css';
 import ReviewStars from '../ReviewStars/ReviewStars'; '../ReviewStars/ReviewStars.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 const PlaceCard = ({ place }) => {
   const [rating, setRating] = useState([]);
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const toggleFavorite = (e) => {
+    e.preventDefault(); // Prevent navigation
+    setIsFavorite(!isFavorite);
+  };
+
 
   return (
     <a href={`/places/${place._id}`} className="place-card">
     <div className="place-card-subContainer">
+    <div className="favorite-icon" onClick={toggleFavorite}>
+    <FontAwesomeIcon className={`${isFavorite ? 'heart-active' : ''}`} color='#48D1B2' icon={faHeart} />
+     </div>
       <img src={place.images[place.images.length - 1]} alt={place.name} className="place-image" />
       <div className="place-details">
         <h2 className="place-name">{place.name}</h2>
