e221dc6a59f4c9c416c8b2f786c96f4391324922
alexandergv <69660314+alexandergv@users.noreply.github.com>
2024-08-05 08:54:52 -0400

    consiguiendo horario de apertura desde la base de datos

--- src/components/PlaceCard/PlaceCard.jsx
+++ src/components/PlaceCard/PlaceCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './PlaceCard.css';
 import ReviewStars from '../ReviewStars/ReviewStars'; '../ReviewStars/ReviewStars.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,12 +7,60 @@ import { faHeart } from '@fortawesome/free-solid-svg-icons';
 const PlaceCard = ({ place }) => {
   const [rating, setRating] = useState([]);
   const [isFavorite, setIsFavorite] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const daysOfWeek = [
+    'sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'
+  ];
+
+  function areTodayHoursEmpty(schedule) {
+    // Obtener el día actual de la semana
+    const now = new Date();
+    const currentDay = daysOfWeek[now.getDay()];
+  
+    // Obtener los horarios de apertura y cierre para el día actual
+    const openTime = schedule[currentDay].open;
+    const closeTime = schedule[currentDay].close;
+  
+    // Verificar si las horas están en blanco
+    return openTime === '' && closeTime === '';
+  }
+  
+  function isPlaceOpen(schedule) {
+    // Obtener el día actual de la semana y la hora actual
+    const now = new Date();
+   
+    const currentDay = daysOfWeek[now.getDay()];
+    const currentTime = now.toTimeString().slice(0, 5); // 'HH:MM' format
+  
+    // Obtener los horarios de apertura y cierre para el día actual
+    const openTime = schedule[currentDay].open;
+    const closeTime = schedule[currentDay].close;
+  
+    // Verificar si el lugar está abierto las 24 horas
+    if (openTime === '24/7') {
+      return true;
+    }
+  
+    // Verificar si el lugar está cerrado todo el día
+    if (openTime === 'Cerrado' || closeTime === 'Cerrado') {
+      return false;
+    }
+  
+    // Verificar si la hora actual está dentro del rango de apertura
+    return currentTime >= openTime && currentTime < closeTime;
+  }
 
   const toggleFavorite = (e) => {
     e.preventDefault(); // Prevent navigation
     setIsFavorite(!isFavorite);
   };
-  const isOpen = place.isOpen; //place.isOpen;
+
+  useEffect(() => {
+    if(isPlaceOpen(place.placeDetails.schedule)){
+      setIsOpen(true);
+    }
+  },[])
 
   return (
     <a href={`/places/${place._id}`} className="place-card">
@@ -20,8 +68,14 @@ const PlaceCard = ({ place }) => {
     <div className="favorite-icon" onClick={toggleFavorite}>
     <FontAwesomeIcon className={`${isFavorite ? 'heart-active' : ''}`} color='#48D1B2' icon={faHeart} />
      </div>
-     <div className={`status-label ${isOpen ? 'open' : 'closed'}`}>
-          {isOpen ? 'Abierto ahora' : 'Cerrado'}
+     <div className={`status-label ${
+      isOpen ? 'open' : 
+      (areTodayHoursEmpty(place.placeDetails.schedule) ? 
+          'unknown' : 'closed')}`
+      }>
+          {isOpen ? 'Abierto ahora' : 
+          (areTodayHoursEmpty(place.placeDetails.schedule) ? 
+          'Horario desconocido' : 'Cerrado')}
       </div>
       <img src={place.images[place.images.length - 1]} alt={place.name} className="place-image" />
       <div className="place-details">
