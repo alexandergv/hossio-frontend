982ed8ace11cc08092186bcb8ec1423d97bdfeb6
alexandergv <69660314+alexandergv@users.noreply.github.com>
2024-09-19 23:58:36 -0400

    places cards on favorite places component

--- src/components/ProfilePanel/UserFavorites.jsx
+++ src/components/ProfilePanel/UserFavorites.jsx
@@ -1,9 +1,32 @@
+
+import axiosInstance from 'services/axiosConfig'
+import { useEffect, useState } from 'react';
+
 const UserFavorites = () => {
-    return (
+  const [favoritePlaces, setFavoritePlaces ] = useState();
+
+  const fetchFavoriteplaces = () => {
+
+  }
+
+  useEffect(() => {
+
+  },[])
+
+  const places = [];
+  return (
       <div>
         <h2>Lugares Destacados</h2>
-        <p>Aquí puedes ver los lugares que has destacado.</p>
-      </div>
+        <div className="place-list">
+        { loading ?
+        (Array(10).fill(0).map((x,index) => <PlaceCardSkeleton key={index} />) )
+        : (places.map(place => (
+          <PlaceCard key={place._id} place={place} />
+            )
+          )
+        )
+        }
+      </div> </div>
     );
   };
   
